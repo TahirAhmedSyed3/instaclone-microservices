d46047a7d762db8534c7cc9ad8ad1b24fea656f2
Tahir Ahmed Syed <tahirahmedsyed3@gmail.com>
2026-03-24 18:19:28 +0300

    Phase 1 - Project structure + PostgreSQL docker setup

---
+++ src/main/java/com/instaclone/App.java
@@ -0,0 +1,13 @@
+package com.instaclone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
